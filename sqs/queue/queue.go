627a91d0658d3c6837edd0707fcd373e69960acf
Rafael <rafael_breno@outlook.com>
2021-10-06 23:14:57 -0300

    feat: Add Push and Pop methods to SQS Queueing

--- sqs/queue/queue.go
+++ sqs/queue/queue.go
@@ -11,12 +11,14 @@ type Queue struct {
 	Common   *internal.Common
 	SQS      *sqs.SQS
 	QueueURL *sqs.GetQueueUrlOutput
+	GroupID  *string
 }
 
 // NewQueue
 func NewQueue(c *internal.Common) (*Queue, error) {
 	q := Queue{
-		Common: c,
+		Common:  c,
+		GroupID: aws.String(c.Env.SQSGroupID),
 	}
 
 	return q.setSQS()
@@ -36,4 +38,31 @@ func (q *Queue) setSQS() (*Queue, error) {
 	q.QueueURL = queueURL
 	q.SQS = s
 	return q, nil
+}
+
+func (q *Queue) Push(msg string) error {
+	_, err := q.SQS.SendMessage(&sqs.SendMessageInput{
+		MessageAttributes: map[string]*sqs.MessageAttributeValue{
+			"message": {
+				DataType:    aws.String("String"),
+				StringValue: aws.String(msg),
+			},
+		},
+		MessageBody:            aws.String("aaaaaaaaaa"),
+		QueueUrl:               q.QueueURL.QueueUrl,
+		MessageGroupId:         q.GroupID,
+		MessageDeduplicationId: q.GroupID,
+	})
+	return err
+}
+
+func (q *Queue) Pop() (*sqs.Message, error) {
+	msgOutput, err := q.SQS.ReceiveMessage(&sqs.ReceiveMessageInput{
+		MessageAttributeNames: []*string{
+			aws.String(sqs.QueueAttributeNameAll),
+		},
+		QueueUrl: q.QueueURL.QueueUrl,
+	})
+
+	return msgOutput.Messages[0], err
 }
