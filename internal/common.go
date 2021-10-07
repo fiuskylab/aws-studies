9122983ffe1ca8d09ec1b0f5cb28691a65f52f10
Rafael <rafael_breno@outlook.com>
2021-10-06 21:49:39 -0300

    feat: Add common

---
+++ internal/common.go
@@ -0,0 +1,44 @@
+package internal
+
+import (
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/joho/godotenv"
+	"go.uber.org/zap"
+)
+
+type Common struct {
+	Session *session.Session
+	Logger  *zap.Logger
+	Env     Env
+}
+
+type Env struct {
+	AWSRegion string
+}
+
+func NewCommon() *Common {
+	l, _ := zap.NewProduction()
+	if err := godotenv.Load(); err != nil {
+		l.Error(err.Error())
+		return nil
+	}
+
+	c := Common{
+		Env: Env{
+			AWSRegion: os.Getenv("AWS_REGION"),
+		},
+		Logger: l,
+	}
+
+	c.Session = session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+		Config: aws.Config{
+			Region: aws.String(c.Env.AWSRegion),
+		},
+	}))
+
+	return &c
+}
